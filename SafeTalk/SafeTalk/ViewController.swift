55b6c8e608065195031ed9c03124edf314415f7e
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-05-13 17:13:49 -0300

    criar projeto no Xcode

---
+++ SafeTalk/SafeTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafeTalk
+//
+//  Created by Gabriel Vicentin Negro on 13/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
