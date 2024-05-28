f61c65d9fb707cac01f7a796155540d34091d3ee
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-05-28 14:57:45 -0300

    tentativa de levantar o teclado apenas na view de criar nota

--- SafeTalk/SafeTalk/Controllers/ViewControllers/Notas/CriarNotaViewController.swift
+++ SafeTalk/SafeTalk/Controllers/ViewControllers/Notas/CriarNotaViewController.swift
@@ -88,6 +88,7 @@ class CriarNotaViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpUI()
+        setupKeyboardHiding()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -165,6 +166,32 @@ class CriarNotaViewController: UIViewController {
         
     }
     
+    //MARK: Funções do teclado
+    
+    private func setupKeyboardHiding(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(sender: NSNotification){
+        guard let userInfo = sender.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+              let currentTextField = UIResponder.currentFirst() as? UITextField else { return }
+        
+        let keyboardTopY = keyboardFrame.cgRectValue.origin.y
+        let convertedTextFieldFrame = view.convert(currentTextField.frame, from: currentTextField.superview)
+        let textFieldBottomY = convertedTextFieldFrame.origin.y + convertedTextFieldFrame.size.height
+        
+        if textFieldBottomY > keyboardTopY {
+            let textBoxY = convertedTextFieldFrame.origin.y
+            let newFrameY = (textBoxY - keyboardTopY / 2) * -1
+            view.frame.origin.y = newFrameY
+        }
+    }
+    @objc func keyboardWillHide(sender: NSNotification){
+        view.frame.origin.y = 0
+    }
+    
     //Funções do CoreData
     func addNota(titulo: String, texto: String){
         //Criar uma nota nova
@@ -181,6 +208,22 @@ class CriarNotaViewController: UIViewController {
         }
     }
 
+}
+
+extension UIResponder {
+    private struct Static {
+        static weak var responder: UIResponder?
+        
+    }
+    @objc private func _trap() {
+        Static.responder = self
+    }
+    
+    static func currentFirst() -> UIResponder? {
+        Static.responder = nil
+        UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+        return Static.responder
+    }
 }
 
 @available(iOS 17.0, *)
